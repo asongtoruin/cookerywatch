946dbdb18591a848fde9f957fa6239b3adb68905
adam <adam.ruszkowski@gmail.com>
2016-12-16 14:26:13 +0000

    First version of light and camera functions

--- raspberry_functions.py
+++ raspberry_functions.py
@@ -1,11 +1,27 @@
 import blinkt
-import picamera
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
 
 def initialise_lights():
-    # TODO - set the information light (light 0) to be green
+    blinkt.clear()
+    blinkt.set_brightness(0.1)
+    change_light(0, 102, 255, 102)
 
-def set_light(r, g, b, pixel):
-    # TODO - set Blinkt pixel to be the colour r,g,b.
+
+def change_light(pixel, r, g, b):
+    blinkt.set_pixel(pixel % 8, r % 256, g % 256, b % 256)
 
 def take_photo():
-    # TODO - take photo, name it according to time and return the output image name to be used to upload to twitter
+    camera = PiCamera()
+    camera.start_preview()
+    change_light(1, 255, 153, 51)
+    sleep(5)
+    output_name = '{}.jpg'.format(datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S'))
+    camera.capture(output_name)
+    return output_name
+
+
+if __name__ == '__main__':
+    print datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')
+
